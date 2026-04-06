a92cf2977df08207d8f9ea3f537276d67484af15
Rungrot Muangchalerm <rungrot.muangchalerm@gmail.com>
2026-04-06 22:43:33 +0700

    user api

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ app.post('/api/register', (req, res) => {
     res.status(200).json({ message: 'register success' })
 })
 
+app.get('/api/user', (req, res) => {
+    res.status(200).json({
+        user: {
+            id: 1,
+            fullName: 'Rungrot Lertprasert',
+            username: 'rungrotl',
+            email: 'rungrot@example.com',
+            phone: '089-123-4567',
+            accountType: 'Personal Plus',
+            memberSince: '2026-01-15',
+            profileImage: 'https://api.dicebear.com/9.x/initials/svg?seed=Rungrot%20L'
+        }
+    })
+})
+
 app.listen(port, () => {
     console.log(`http://localhost:3000`)
 })
